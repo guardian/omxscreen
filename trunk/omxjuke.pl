4ab1213555ebe86e04ea9d6185a35e4c58deef3e
andyg <andyg@1f3f007e-cecb-4ff2-a483-6d09a5540f9b>
2013-05-08 22:03:04 +0000

    Small tweaks to open platform jukebox player

--- trunk/omxjuke.pl
+++ trunk/omxjuke.pl
@@ -7,6 +7,7 @@ use Data::Dumper;
 my $queryurl='http://content.guardianapis.com/search?tag=music%2Fmusic%2Ctype%2Fvideo%2Ctone%2Fperformances&format=json&show-tags=all&show-media=all&api-key=techdev-internal';
 
 our @target_encodings=('video/mp4:720','video/3gp:large');
+our $playerargs='-o hdmi';
 
 sub find_best_encoding {
 my $record=shift;
@@ -44,15 +45,21 @@ for(my $page=1;$page<100;++$page){
 
 	my $data=from_json($jsoncontent);
 #print Dumper($data);
-
+	my $rc;
 	foreach(@{$data->{'response'}->{'results'}}){
 		#print find_encoding('video/mp4:720',$_);
 		my $url=find_best_encoding($_);
 		print $url;
 		print $_->{'webTitle'}."\n----------------------------\n";
 #		print Dumper($_);
-		system("/usr/bin/omxplayer \"$url\"");
-		print "omxplayer returned $?\n";
-		last if($?>0);
+		unless($url){
+			print "An applicable URL was not found.\n";
+			next;
+		}
+		system("/usr/bin/omxplayer $playerargs \"$url\"");
+		$rc=$?;
+		print "omxplayer returned $rc\n";
+		last if($rc>0);
 	}
+	last if($rc>0);
 }
